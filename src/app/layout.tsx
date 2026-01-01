5a4cc61444d126fd604938220dd7575cbba0730f
terashimasatoshi <terashima@minervacosmetics.jp>
2026-01-01 17:46:21 +0900

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
   description: "あなたの疲れタイプを診断して、最適なスパメニューをご提案します。",
 };
 
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,7 +22,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KTSDFND5');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${zenMaru.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KTSDFND5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
